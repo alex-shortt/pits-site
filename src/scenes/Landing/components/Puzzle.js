5e357e6847cdac7e6390b40818991c1f6338e07f
Alex Shortt <alexander.shortt@gmail.com>
2020-05-29 11:24:27 -0700

    add styles / logic for piece movement

--- src/scenes/Landing/components/Puzzle.js
+++ src/scenes/Landing/components/Puzzle.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components/macro"
 import { useSpring, animated } from "react-spring"
 import { useGesture } from "react-with-gesture"
@@ -62,33 +62,57 @@ const PieceImg = styled.img.attrs({ src: missingPieceImg })`
   width: 100%;
   height: 100%;
   object-fit: contain;
-  transition: all 0.15s linear;
+  transition: all 0.25s linear;
   pointer-events: none;
+  ${props => props.dragging && "transform: scale(1.1)"};
+  ${props => props.close && "filter: brightness(1.4);"};
 `
 
 const ShadowImg = styled(PieceImg)`
   filter: brightness(0.3);
+  ${props => props.dragging && "transform: scale(1.11)"};
+  ${props => props.dragging && "filter: brightness(0.2) blur(8px);"};
 `
 
 export default function Puzzle(props) {
-  // x,y offset
+  // still, moving, close, done
+  const [pieceState, setPieceState] = useState("still")
+
+  // x,y offset represented as a decimal from -0.5 to 0,5
+  // offset from 0,0 being center of screen
+  // multiplied by screen width/height in a sec
   const [{ xy }, set] = useSpring(() => {
     const startX = (Math.random() - 0.5) * 0.6 + 0.15
     const startY = (Math.random() - 0.5) * 0.6 + 0.15
 
     return {
-      xy: [startX * window.innerWidth, startY * window.innerHeight]
+      xy: [startX, startY]
     }
   })
 
   const bind = useGesture(opts => {
     const { down, delta, velocity, previous } = opts
+
     const newVelocity = clamp(velocity, 1, 4)
+    let newX = (previous[0] - window.innerWidth / 2) / window.innerWidth
+    let newY = (previous[1] - window.innerHeight / 2) / window.innerHeight
+
+    const dist = Math.sqrt(Math.pow(newX, 2) + Math.pow(newY, 2))
+
+    if (dist < 0.1 && !down) {
+      newX = 0
+      newY = 0
+      setPieceState("done")
+    } else if (dist < 0.1 && down) {
+      setPieceState("close")
+    } else if (!down) {
+      setPieceState("still")
+    } else if (down) {
+      setPieceState("moving")
+    }
+
     set({
-      xy: [
-        previous[0] - window.innerWidth / 2,
-        previous[1] - window.innerHeight / 2
-      ],
+      xy: [newX, newY],
       config: { mass: newVelocity, tension: 500 * newVelocity, friction: 50 }
     })
   })
@@ -100,13 +124,20 @@ export default function Puzzle(props) {
         {...bind()}
         style={{
           transform: xy.interpolate(
-            (x, y) => `translate(-50%, -50%) translate3d(${x}px,${y}px,0)`
+            (x, y) =>
+              `translate(-50%, -50%) translate3d(${x *
+                window.innerWidth}px,${y * window.innerHeight}px,0)`
           )
         }}
       >
         <div>
-          <ShadowImg />
-          <PieceImg />
+          <ShadowImg
+            dragging={pieceState === "moving" || pieceState === "close"}
+          />
+          <PieceImg
+            dragging={pieceState === "moving" || pieceState === "close"}
+            close={pieceState === "close" || pieceState === "done"}
+          />
         </div>
       </PuzzlePiece>
     </>
